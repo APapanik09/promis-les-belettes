65c62946cbddb0e62523f035abaf7f16086d4379
Jolyanne Desjardins <113621167+jolyanneDesgardins@users.noreply.github.com>
2023-10-05 15:58:55 -0400

    arranger swipper ish

--- script.js
+++ script.js
@@ -18,4 +18,17 @@ const swiper = new Swiper(".swiper", {
   scrollbar: {
     el: ".swiper-scrollbar",
   },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    // when window width is >= 640px
+  },
 });
