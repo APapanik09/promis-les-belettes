afb95cd69532ac16c04ac1f68a55652ed1405afd
lauriehoude <2052728@cmontmorency.qc.ca>
2023-10-23 18:17:02 -0400

    arranger l'animation ligne du temps de la page histoire

--- script.js
+++ script.js
@@ -45,20 +45,25 @@ const enveloppe= document.querySelectorAll(".section-histoire ");
 
 
 gsap.set(".section-histoire", {autoAlpha: 0});
-gsap.set(".timeline", {autoAlpha: 1});
+gsap.set(".promis", {autoAlpha: 0});
+gsap.set(".creation", {autoAlpha: 0});
+gsap.set(".MariaGoretti", {autoAlpha:0});
 gsap.timeline( { 
   x: '100%',
   scrollTrigger: {
     pin: true,
     scrub: true,
-    start: 'top 15%',
+    start: 'top 5%',
     end: 'bottom 5%',
     trigger: '.timeline',
 
   },
 })
-.to('.promis', { x: '1300px', duration:2, delay:2 },'<0.5')
-.to('.creation',{ x: '1000px', duration:2, delay:3},'<0.5')
-.to('.MariaGoretti',{ x: '1000px', duration:2, delay:4},'< 1')
-.to('.section-histoire',{ autoAlpha: 1, y:'-50%', ease: 'linear', duration:3})
-.to('.timeline',{ autoAlpha: 0, y:'-50%', ease: 'linear', duration:3})
+.to('.promis', { autoAlpha: 1,x: '-200px', duration:10, delay:2, ease: 'linear'},'<0.5')
+.to('.creation',{autoAlpha: 1, x: '-100px', duration:10, delay:3, ease: 'linear'},'>0.5')
+.to('.MariaGoretti',{ autoAlpha: 1,x: '-50px', duration:10, delay:4, ease: 'linear'},'> 1')
+.to('.promis', { autoAlpha: 0,x: '-200px', duration:6, delay:2, ease: 'linear' },'<0.5')
+.to('.creation',{autoAlpha: 0, x: '-100px', duration:6, delay:3, ease: 'linear'},'<0.5')
+.to('.MariaGoretti',{ autoAlpha: 0,x: '-50px', duration:6, delay:4, ease: 'linear'},'< 1')
+.to('.section-histoire',{ autoAlpha: 1, y:'-50%', ease: 'linear', duration:6})
+
