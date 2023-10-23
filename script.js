1261868891e07f5564d6bb3f02966d36b09ca7ff
lauriehoude <2052728@cmontmorency.qc.ca>
2023-10-23 14:58:17 -0400

    animation la page histoire

--- script.js
+++ script.js
@@ -42,20 +42,19 @@ const MariaGoretti = document.querySelector('.MariaGoretti');
 const enveloppe= document.querySelectorAll(".section-histoire ");
 
 
-
+gsap.set(".section-histoire", {autoAlpha: 0});
 gsap.timeline( { 
   x: '100%',
   scrollTrigger: {
     pin: true,
     scrub: true,
-    markers: true,
     start: 'top 15%',
-    end: 'bottom 25%',
+    end: 'bottom 5%',
     trigger: '.timeline',
 
   },
 })
 .to('.promis', { x: '1300px', duration:2 },'<0.5')
 .to('.creation',{ x: '1000px'},'<0.5')
-.to('.MariaGoretti',{ x: '1000px'},'<0.5')
-.to('.enveloppe',{ diplay: "block"},'<0.5')
+.to('.MariaGoretti',{ x: '1000px'},'< 1')
+.to('.section-histoire',{ autoAlpha: 1, })
