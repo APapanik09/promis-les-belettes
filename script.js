e83ea8156ac654e66a6ca6f05e9119bec1bae3c3
Jolyanne Desjardins <113621167+jolyanneDesgardins@users.noreply.github.com>
2023-10-29 20:32:21 -0400

    je suis tanné

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+const swiperCarrousel = new Swiper(".swiperCarrousel", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: ".swiper-pagination",
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: ".swiper-scrollbar",
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        // when window width is >= 480px
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+        },
+        // when window width is >= 640px
+    },
+});
 const swiper = new Swiper(".swiper", {
     // Optional parameters
     direction: "horizontal",
