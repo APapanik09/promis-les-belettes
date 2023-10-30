5475767f6e3d366349b5dc155e27f305fdf92cf4
Jolyanne Desjardins <113621167+jolyanneDesgardins@users.noreply.github.com>
2023-10-30 11:33:23 -0400

    essayer de changer le hero

--- script.js
+++ script.js
@@ -1,32 +1,18 @@
-/* (A RETRAVAILLER) 
-const swiperCarrousel = new Swiper(".swiperCarrousel", {
+
+const swiperCarrousel = new Swiper(".swiperCaroussel", {
     // Optional parameters
     direction: "horizontal",
     loop: true,
 
-    // If we need pagination
-    pagination: {
-        el: ".swiper-pagination",
-    },
+
+   
 
     // And if we need scrollbar
     scrollbar: {
         el: ".swiper-scrollbar",
     },
-    breakpoints: {
-        // when window width is >= 320px
-        320: {
-            slidesPerView: 1,
-            spaceBetween: 10,
-        },
-        // when window width is >= 480px
-        768: {
-            slidesPerView: 3,
-            spaceBetween: 10,
-        },
-        // when window width is >= 640px
-    },
-});*/
+   
+});
 const swiper = new Swiper(".swiper", {
     // Optional parameters
     direction: "horizontal",
