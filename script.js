f2cc950132d4fc4c7aaf0d54b7dbbee9465f50e5
APapanik09 <89647801+APapanik09@users.noreply.github.com>
2023-11-21 11:05:33 -0500

    fixed animation quatre

--- script.js
+++ script.js
@@ -185,24 +185,13 @@ if (don) {
 }
 /* fin animation au click*/
 
-// Motionpath anim 404
+//anim 404
 
-let animquatre =
-
-    gsap.to('.birdfour', {
-        motionPath: {
-            path: '.volbird',
-            align: '.volbird',
-            autoRotate: true,
-            start: 0,
-            end: 0.55,
-            alignOrigin: [0.5, 0.7],
-            xPercent: 50,
-            yPercent: 90,
-        },
-        duration: 10,
-        yoyo: true,
-    });
+gsap.timeline()
+.to('.oiseauquatre', { x: '-120vw', duration: 4 })
+.to('.oiseauquatre_deux', { x: '-120vw',duration: 4 },)
+.to('.oiseauquatre_trois', { x: '-120vw', duration: 4 }, "<-1")
+.to('.oiseauquatre_quatre', { x: '-120vw', duration: 4 }, "<-2");
 
 // Modal Equipe
 //$('#myModal').modal(options)
