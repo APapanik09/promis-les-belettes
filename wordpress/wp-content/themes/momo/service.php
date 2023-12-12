d7684ec3a64b74aecf67397dde6fc4eb12287e06
Jolyanne Desjardins <113621167+jolyanneDesgardins@users.noreply.github.com>
2023-12-12 18:34:42 -0500

    ajout des page jolyanne

--- wordpress/wp-content/themes/momo/service.php
+++ wordpress/wp-content/themes/momo/service.php
@@ -1,37 +1,35 @@
 <?php 
 /**
- *  Template Name: service
- * Template Post Type: carte-service
- *  Identique à page, mais avec une barre latérale
+ * 	Template Name: service
+  * Template Post Type: carte-service
+ * 	Identique à page, mais avec une barre latérale
  */
 
 get_header(); // Affiche header.php
 
 if ( have_posts() ) : // Est-ce que nous avons des pages à afficher ? 
-    // Si oui, bouclons au travers les pages (logiquement, il n'y en aura qu'une)
-    while ( have_posts() ) : the_post(); 
+	// Si oui, bouclons au travers les pages (logiquement, il n'y en aura qu'une)
+	while ( have_posts() ) : the_post(); 
 ?>
 
-<article>
-    <?php if (!is_front_page()) : // Si nous ne sommes PAS sur la page d'accueil ?>
-    <h2>
 
-        <?php endif; ?>
-
-
-</article>
 <?php endwhile; // Fermeture de la boucle
 
 else : // Si aucune page n'a été trouvée
-    get_template_part( 'partials/404' ); // Affiche partials/404.php
+	get_template_part( 'partials/404' ); // Affiche partials/404.php
 endif;
 
 ?>
 
 
+<?php
+  $projects = new WP_Query('post_type= carte-service');
+  while ($projects->have_posts()) : $projects->the_post(); 
+?>
 
-
-
+<?php   endwhile;
+  wp_reset_postdata();
+?>
 
 <section class="detail">
 
@@ -39,10 +37,7 @@ endif;
         <div class="row align-items-center">
             <div class=" col-12 text-lg-start">
                 <div class="titre-a-propos ">
-                    <?php
-  $projects = new WP_Query('post_type= carte-service');
-  while ($projects->have_posts()) : $projects->the_post(); 
-?>
+
 
                     <h1 class="titre-a-propos col-sm-12 col-12"><?php the_title()?></h1>
                     <h3 class="texte-hero-a-propos col-12"> <?php the_content()?>
@@ -100,75 +95,89 @@ endif;
 
         <div class="row row__service ">
             <!--Premier Service-->
+            <?php if(get_field("titre_sous_service_1") != ''):?>
             <div class="card  service__accueil autres d-none  m-2 col-sm-12 col-md-6 col-3 d-lg-block">
                 <div class="card-body ">
                     <div class="row ">
                         <h3 class="card-title"><?php the_field("titre_sous_service_1")?></h3>
 
                     </div>
-                    <h4 class="card-text"><?php the_field("texte_sous_service_1")?> </h4>
+                    <p class="card-text"><?php the_field("texte_sous_service_1")?> </p>
 
                 </div>
             </div>
+            <?php   endif;?>
             <!--Deuxième Service-->
+            <?php if(get_field("titre_sous_service_2") != ''):?>
             <div class="card service__accueil autres  m-2 col-sm-12 col-md-6 col-3 d-lg-block ">
                 <div class="card-body ">
                     <h3 class="card-title">
                         <?php the_field("titre_sous_service_2")?></h3>
 
-                    <h4 class="card-text"><?php the_field("texte_sous_service_2")?>
+                    <p class="card-text"><?php the_field("texte_sous_titres_2")?>
 
-                    </h4>
+                    </p>
 
                 </div>
             </div>
-
+            <?php   endif;?>
             <!--troisième Service-->
+            <?php if(get_field("titre_sous_service_3") != ''):?>
             <div class="card service__accueil autres  m-2 col-sm-12 col-md-6 col-3 d-lg-block d-lg-block  ">
                 <div class="card-body ">
                     <h3 class="card-title"><?php the_field("titre_sous_service_3")?></h3>
 
-                    <h4 class="card-text"><?php the_field("texte_sous_service_3")?>
-                    </h4>
+                    <p class="card-text"><?php the_field("texte_sous_services_3")?>
+                    </p>
 
                 </div>
             </div>
-
+            <?php   endif;?>
             <!--quatrième service-->
+            <?php if(get_field("titre_sous_service_4") != ''):?>
             <div class="card  service__accueil autres m-2 col-sm-12 col-md-6 col-3 d-lg-block">
                 <div class="card-body ">
                     <h3 class="card-title"><?php the_field("titre_sous_service_4")?></h3>
 
-                    <h4 class="card-text"><?php the_field("texte_sous_service_4")?></h4>
+                    <p class="card-text"><?php the_field("texte_sous_services_4")?></p>
 
                 </div>
             </div>
+            <?php   endif;?>
+        </div>
 
+        <!--cinquième service-->
+        <?php if(get_field("titre_sous_service_5") != ''):?>
+        <div class="card  service__accueil autres m-2 col-sm-12 col-md-6 col-3 d-lg-block">
+            <div class="card-body ">
+                <h3 class="card-title"><?php the_field("titre_sous_service_5")?></h3>
 
+                <p class="card-text"><?php the_field("texte_sous_services_5")?></p>
+
+            </div>
+        </div>
+        <?php   endif;?>
+    </div>
 </section>
 <section class="surplus">
     <div class="webserie-balado d-grid">
         <div class="web-serie col-12">
             <h3><?php the_field("titre_video")?></h3>
             <div class="video col-12">
-                <iframe src="https://www.youtube.com/watch?v=lO-t5AyRoCg&t=3s" frameborder="0"></iframe>
+                <iframe src="<?php the_field("lien_video")?>" frameborder="0"></iframe>
             </div>
         </div>
         <div class="balado">
             <h3 class="col-12"><?php the_field("titre_balados")?></h3>
-            <audio class="col-12" controls
-                src="https://www.buzzsprout.com/1979567/10626818-service-d-accueil-et-integration.mp3?client_source=small_player&referrer=https%3A%2F%2Fwww.buzzsprout.com%2F1979567%2F10626818-accueil-et-integration.js%3Fcontainer_id%3Dbuzzsprout-player-10626818"></audio>
+            <audio class="col-12" controls src="<?php the_field("lien_balados")?>"></audio>
 
         </div>
     </div>
+
 </section>
 
-<?php   endwhile;
-  wp_reset_postdata();
-?>
+
 <?php
 
 get_footer(); // Affiche footer.php 
 ?>
-Réduire
-Dispose d’un menu contextuel
